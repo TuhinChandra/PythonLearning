44eba366e44bf79f978a49918276ceed5225af74
TuhinChandra <santuc1990@gmail.com>
2020-04-13 18:56:32 +0530

    Usage of Print

--- Basics/Print_Function/Tuhin/print_example.py
+++ Basics/Print_Function/Tuhin/print_example.py
@@ -1,1 +1,10 @@
-print("Hello World")
+#WAP to print multiple strings, an int and a float value using a single print statement in python.
+#exampleOutput: Hello... Second String... int value: 10 float value: 55.15
+firstString="First String"
+secondString="Second String"
+integerValue=10
+floatvalue=55.55
+print(firstString,secondString,integerValue,floatvalue,sep='...')
+#WAP to print multiple strings with a proper string difference without printing the pattern manually.
+#exampleOutput:
+# Hello...**mySeparator**Second String...**mySeparator**10**mySeparator**55.15**mySeparator**Third too...
